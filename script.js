b4c0520f39d91798275b08d60d8ff00a7a8edcca
NataliiaKovaliv <nataliiakovaliv95@gmail.com>
2021-09-15 13:01:52 +0300

    refactor: add self-esteem

---
+++ script.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector('.sidebar__btn');
+let menu = document.querySelector('.sidebar__menu');
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle('active')
+  btn.classList.toggle('active')
+})
+
+
+
+console.log('Total score: 150/150')
+console.log('Html file is valid, no errors or warnings 10/10')
+console.log('Layout is semantic, tags that I used: aside, nav, main, section, footer, h1, h2, h3, h4, h5, h6 20/20')
+console.log('Css style is used 10/10')
+console.log('Content is placed in a block and horizontally centered on the page. The background color stretches to the full width of the page 10/10')
+console.log('Layout is adaptive 10/10')
+console.log('Adaptive burger-menu and smooth scrolling by anchors 10/10')
+console.log('There is a photo on the page, image proportions are not distorted. There is attribute "alt" 10/10')
+console.log('Contacts and skills are presented in the form of a list ul > li 10/10')
+console.log('CV contains contacts, information about myself, a list of skills, information about education and level of English 10/10')
+console.log('There is a code example 10/10')
+console.log('CV contains image links to the projects that I have done. When you click on the image, the project page opens in a new tab. Each project has a name and a list of technologies that I used. I didn\'t add a short description, but I added button wich open project on GitHub and here you can see the code and description 10/10')
+console.log('CV is written in English 10/10')
+console.log('Pull Request requirements are met 10/10')
+console.log('There is a video summary in English and transcription to it 0/10')
+console.log('I think that design and quality of the CV is not lower than in the examples 10/10')
